688f4100631233c6203514853572a6ad0dc1b7e2
Jyoti Gupta <jyotigupta275@gmail.com>
2016-05-25 15:41:00 -0500

    Initial git commit

---
+++ conf/appc.arrowdb.production.js
@@ -0,0 +1,16 @@
+/**
+ * Generated Arrow DB configuration for production
+ */
+module.exports = {
+  "connectors": {
+    "appc.arrowdb": {
+      "enabled": true,
+      "connector": "appc.arrowdb",
+      "environment": "production",
+      "key": "DHfOXA6sQ6PkBqGQPfaeY7s5DYSaiUkb",
+      "baseurl": "https://api.cloud.appcelerator.com",
+      "username": "appc_app_user",
+      "password": "jQ9u2TmdSjPPzLgn74"
+    }
+  }
+};
